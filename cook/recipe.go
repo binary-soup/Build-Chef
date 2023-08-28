f3e062e5a16a4e51eeffbb911fd3e7842ed47788
Ryan <mhogar98@gmail.com>
2023-08-28 19:36:41 -0400

    Implement compiler command with coloured output

--- cook/recipe.go
+++ cook/recipe.go
@@ -5,7 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+
+	"github.com/binary-soup/bchef/style"
 )
 
 type recipe struct {
@@ -30,6 +33,26 @@ func newRecipe(path string) (*recipe, error) {
 	}, nil
 }
 
-func (r *recipe) Cook() {
-	fmt.Println(r.Name)
+func (r *recipe) exec() bool {
+	//NOTE: add -g for debug
+	//TODO: handle command injection
+
+	cmd := exec.Command("g++", "-Wall", "-std=c++17", "-o", r.Name, "main.cxx")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	_, ok := cmd.Run().(*exec.ExitError)
+	return ok
+}
+
+func (r *recipe) Cook() bool {
+	fmt.Print(style.New(style.Magenta).Format("main.cxx"), " -> ")
+	isExitErr := r.exec()
+
+	if isExitErr {
+		return false
+	}
+
+	style.New(style.Green, style.Bold).Println(r.Name)
+	return true
 }
---
+++ style/style.go
@@ -0,0 +1,38 @@
+package style
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	Bold      = "1"
+	Underline = "4"
+	Red       = "31"
+	Green     = "32"
+	Yellow    = "33"
+	Blue      = "34"
+	Magenta   = "35"
+	Cyan      = "36"
+)
+
+type Style []string
+
+func New(style ...string) Style {
+	return style
+}
+
+func (s Style) Format(str string) string {
+	if len(s) == 0 {
+		return str
+	}
+	return "\033[" + strings.Join(s, ";") + "m" + str + "\033[0m"
+}
+
+func (s Style) Print(str string) {
+	fmt.Print(s.Format(str))
+}
+
+func (s Style) Println(str string) {
+	fmt.Println(s.Format(str))
+}
