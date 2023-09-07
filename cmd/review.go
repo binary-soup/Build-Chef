f63663e5f5579e16cecbb3f47f0c6a265c50956e
Ryan <mhogar98@gmail.com>
2023-09-07 18:34:26 -0400

    Add (partial) library verification

--- cmd/review.go
+++ cmd/review.go
@@ -17,6 +17,7 @@ func NewReviewCommand() ReviewCommand {
 
 type ReviewCommand struct {
 	command
+	verify bool
 }
 
 func (cmd ReviewCommand) Run(args []string) error {
@@ -29,59 +30,120 @@ func (cmd ReviewCommand) Run(args []string) error {
 		return err
 	}
 
-	cmd.info(r, *verify)
+	cmd.verify = *verify
+	cmd.review(r)
+
 	return nil
 }
 
-func (cmd ReviewCommand) info(r *recipe.Recipe, verify bool) {
+func (cmd ReviewCommand) review(r *recipe.Recipe) {
 	style.Header.Println("Executable:")
-	cmd.reviewExecutable(r, r.Executable, verify)
-	cmd.reviewSourceFile(r, r.MainSource, verify)
+	cmd.reviewExecutable(r, r.Executable)
+	cmd.reviewSourceFile(r, r.MainSource)
 
 	style.Header.Println("Source Files:")
 	for _, src := range r.SourceFiles {
-		cmd.reviewSourceFile(r, src, verify)
+		cmd.reviewSourceFile(r, src)
 	}
 
 	style.Header.Println("Include Directories:")
 	for _, include := range r.Includes {
-		style.InfoV2.Println(INDENT + include)
+		cmd.reviewSystemPath(include)
 	}
 
 	style.Header.Println("Library Paths:")
 	for _, path := range r.LibraryPaths {
-		style.InfoV2.Println(INDENT + path)
+		cmd.reviewSystemPath(path)
 	}
 
 	style.Header.Println("Libraries:")
 	for _, lib := range r.Libraries {
-		style.FileV2.Println(INDENT + lib)
+		cmd.reviewLibrary(lib, append(r.LibraryPaths, "."))
 	}
 }
 
-func (cmd ReviewCommand) reviewExecutable(r *recipe.Recipe, exec string, verify bool) {
-	cmd.reviewFile(style.BoldCreate, exec, r.JoinPath(filepath.Dir(exec)), "path", verify)
-}
-
-func (cmd ReviewCommand) reviewSourceFile(r *recipe.Recipe, src string, verify bool) {
-	cmd.reviewFile(style.File, src, r.JoinPath(src), "file", verify)
-}
-
-func (cmd ReviewCommand) reviewFile(style style.Style, file string, filepath string, name string, verify bool) {
+func (cmd ReviewCommand) reviewFilepath(verify func() bool, pass string, fail string, entry string) {
 	fmt.Print(INDENT)
 
-	if verify {
-		cmd.verifyPath(filepath, name)
+	if cmd.verify {
+		if verify() {
+			fmt.Print(pass)
+		} else {
+			fmt.Print(fail)
+		}
 	}
-	style.Println(file)
+
+	fmt.Println(entry)
 }
 
-func (ReviewCommand) verifyPath(path string, name string) {
-	_, err := os.Stat(path)
+func (cmd ReviewCommand) reviewExecutable(r *recipe.Recipe, exec string) {
+	cmd.reviewFilepath(
+		cmd.verifyPath(r.JoinPath(filepath.Dir(exec))),
+		cmd.verified(), cmd.notFound("path"),
+		style.BoldCreate.String(exec),
+	)
+}
 
-	if err == nil {
-		style.Success.Print("[verified] ")
-	} else {
-		style.Error.Printf("[%s not found] ", name)
+func (cmd ReviewCommand) reviewSourceFile(r *recipe.Recipe, src string) {
+	cmd.reviewFilepath(
+		cmd.verifyPath(r.JoinPath(src)),
+		cmd.verified(), cmd.notFound("file"),
+		style.File.String(src),
+	)
+}
+
+func (cmd ReviewCommand) reviewSystemPath(path string) {
+	cmd.reviewFilepath(
+		cmd.verifyPath(path),
+		cmd.verified(), cmd.notFound("path"),
+		style.InfoV2.String(path),
+	)
+}
+
+func (cmd ReviewCommand) verifyPath(path string) func() bool {
+	return func() bool {
+		return cmd.pathExists(path)
 	}
+}
+
+func (ReviewCommand) pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func (cmd ReviewCommand) reviewLibrary(lib string, paths []string) {
+	cmd.reviewFilepath(
+		cmd.verifyLibrary(paths, lib),
+		cmd.verified(), cmd.unverified(),
+		style.FileV2.String(lib),
+	)
+}
+
+func (cmd ReviewCommand) verifyLibrary(paths []string, lib string) func() bool {
+	return func() bool {
+		return cmd.libraryExists(paths, lib)
+	}
+}
+
+func (cmd ReviewCommand) libraryExists(paths []string, lib string) bool {
+	for _, path := range paths {
+		for _, ext := range []string{".a", ".so"} {
+			if cmd.pathExists(filepath.Join(path, "lib"+lib+ext)) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (ReviewCommand) verified() string {
+	return style.Success.String("[verified] ")
+}
+
+func (ReviewCommand) unverified() string {
+	return style.Info.String("[unverified] ")
+}
+
+func (ReviewCommand) notFound(name string) string {
+	return style.Error.Format("[%s not found] ", name)
 }
